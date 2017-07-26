27193db7bc0f99b46fea38973260634544c39c69
Colby Martin <colbymartin@Colbys-MacBook-Air.local>
2017-07-26 13:38:37 -0400

    setup

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browser = require('gulp-browser');
+
+let OUTPUT_DIRECTORY = 'public';
+
+// html, css, and js tasks
+
+gulp.task('default', ['html', 'css', 'js', 'templates', 'assets']);
+
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(gulp.dest(OUTPUT_DIRECTORY));
+});
+
+gulp.task('templates', function () {
+    return gulp.src('templates/*.html')
+        .pipe(gulp.dest(OUTPUT_DIRECTORY + '/templates'));
+});
+
+gulp.task('css', function () {
+    return gulp.src('scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(OUTPUT_DIRECTORY));
+});
+
+
+gulp.task('js', function () {
+    return gulp.src('js/app.js')
+        .pipe(browser.browserify())
+        .pipe(gulp.dest(OUTPUT_DIRECTORY));
+});
+
+gulp.task('assets', function () {
+    return gulp.src('assets/**')
+        .pipe(gulp.dest(OUTPUT_DIRECTORY + '/assets'));
+});
+
+
+gulp.task('watch', ['default'], function () {
+    gulp.watch('index.html', ['html']);
+    gulp.watch('scss/*.scss', ['css']);
+    gulp.watch('js/*.js', ['js']);
+    gulp.watch('js/*/*.js', ['js']);
+    gulp.watch('templates/*.html', ['templates']);
+    gulp.watch('assets/**', ['assets']);
+});
---
+++ js/app.js
@@ -0,0 +1,91 @@
+const app = angular.module('BrewCLT', ['ui.router']);
+
+const controllers = [
+    require('./controllers/main'),
+];
+
+for (let i = 0; i < controllers.length; i++) {
+    app.controller(controllers[i].name, controllers[i].func);
+};
+
+const breweryService = require('./services/breweries');
+app.factory(breweryService.name, breweryService.func);
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider.state({
+        name: 'home',
+        url: '/home',
+        component: 'homePage',
+    });
+
+    $stateProvider.state({
+        name: 'breweries',
+        url: '/breweries',
+        component: 'breweriesPage',
+    });
+
+    $stateProvider.state({
+        name: 'map',
+        url: '/map',
+        component: 'mapPage',
+    });
+
+});
+
+app.component('homePage', {
+    templateUrl: 'templates/home.html',
+    controller: 'MainController',
+});
+
+app.component('news', {
+    templateUrl: 'templates/news.html',
+    controller: 'MainController',
+});
+
+app.component('articles', {
+    templateUrl: 'templates/articles.html',
+    controller: 'MainController',
+});
+
+app.component('monthly', {
+    templateUrl: 'templates/monthly.html',
+    controller: 'MainController',
+});
+
+app.component('breweriesPage', {
+    templateUrl: 'templates/breweries.html',
+    controller: 'MainController',
+});
+
+app.component('mapPage', {
+    templateUrl: 'templates/map.html',
+    controller: 'MainController',
+});
+
+app.component('header', {
+    templateUrl: 'templates/header.html',
+    controller: 'MainController',
+});
+
+app.component('brewfooter', {
+    templateUrl: 'templates/footer.html',
+    controller: 'MainController',
+});
+
+app.component('about', {
+    templateUrl: 'templates/about.html',
+    controller: 'MainController',
+});
+
+app.component('social', {
+    templateUrl: 'templates/social.html',
+    controller: 'MainController',
+});
+
+app.component('social', {
+    templateUrl: 'templates/social.html',
+    controller: 'MainController',
+});
